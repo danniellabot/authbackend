c6dbef64c529181e9c272867b233dcd51b3522bb
danniellabot <danniellanas@gmail.com>
2021-05-10 21:27:07 +0100

    encryption and login check if user already exists

--- src/api/users.js
+++ src/api/users.js
@@ -1,28 +1,65 @@
-import express from 'express'
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-import validateRegisterInput from '../validation/register'
-import validateLoginInput from '../validation/login'
-import users from '../models/User'
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import validateRegisterInput from "../validation/register";
+import validateLoginInput from "../validation/login";
+import Users from "../models/User";
 
+const saltRounds = 10
 
-const router = express.Router()
+const router = express.Router();
 
 router.post("/register", async (req, res) => {
-    try {
-        const newUser = new users({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-     
-        })
-        await newUser.save()
-        res.send(newUser)
-    }
-    catch(err) {
-        console.log(err)
-    }
-  
+  try {
+
+    const hashed = await bcrypt.hash(req.body.password, saltRounds)
+
+    const newUser = new Users({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashed,
+    });
+
+    await newUser.save();
+    res.send(newUser);
+
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-module.exports = router
+router.post("/login", async (req, res) => {
+    try {
+  
+      const user = await Users.findOne({
+          email: req.body.email
+      })
+      console.log(user)
+      if (user) {
+        const cmp = await bcrypt.compare(req.body.password, user.password);
+        if (cmp) {
+            const payload = {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+            }
+            const accessToken = jwt.sign(payload, process.env.SECRET_OR_KEY)
+            res.json({
+                success: true,
+                token: "Bearer " + accessToken
+              })
+          //   ..... further code to maintain authentication like jwt or sessions
+          //res.send("Auth Successful");
+        } else {
+          res.send("Wrong username or password.");
+        }
+      } else {
+        res.send("Wrong username or password.");
+      }
+  
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+module.exports = router;
